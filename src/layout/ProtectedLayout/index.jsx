2141a59447533b3725808f02023f6397c96d8b5b
meet <meetbhungaliya1900@gmail.com>
2025-09-10 21:44:30 +0530

    removed auth loader

--- src/layout/ProtectedLayout/index.jsx
+++ src/layout/ProtectedLayout/index.jsx
@@ -1,17 +1,17 @@
-import Loading from "@/components/common/Loading";
+// import Loading from "@/components/common/Loading";
 import { useAuthStore } from "@/hooks/use-auth";
 import { Navigate, Outlet } from "react-router";
 
 const ProtectedLayout = () => {
   const authState = useAuthStore();
 
-  if (authState.isLoading) {
-    return (
-      <div className="w-full h-dvh flex justify-center items-center">
-        <Loading className="animate-spin" />
-      </div>
-    );
-  }
+  // if (authState.isLoading) {
+  //   return (
+  //     <div className="w-full h-dvh flex justify-center items-center">
+  //       <Loading className="animate-spin" />
+  //     </div>
+  //   );
+  // }
 
   if (!authState.isAuthenticated) {
     return <Navigate to="/login" replace />;
